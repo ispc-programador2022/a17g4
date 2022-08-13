335ea32d315af73bf241eba38faf45166da7ad5b
Agustin-Reyna <106390161+Agustin-Reyna@users.noreply.github.com>
2022-08-13 19:51:50 -0300

    Ejercicio Pyhton

---
+++ AgustinReyna-Ejercicio-5-Python.py
@@ -0,0 +1,7 @@
+Historial = (2350, 5960, 23000, 1000, 8900)
+
+GastosPuppy = sum(list(Historial)) 
+if GastosPuppy < 30000:
+    print(GastosPuppy)
+else:
+   print("Gastos por encima de lo presupuestado.")
